2f7ed32903a4d0b32df46264e5bbf5deab51a624
Maria Candelaria Fernandez <candelariafeern@gmail.com>
2025-03-25 16:22:55 -0300

    Modelo

--- javascript.js
+++ javascript.js
@@ -4,10 +4,8 @@ let menuOpenButton = document.querySelector ('#menu-open-button')
 
 menuCloseButtton.onclick = () => {
     menulist.style.setProperty("display", "none")
-    console.log("asd")    
     // neceitamos el menu
     // tenemos que ponerle display none al menu
-
     // menu.classList.toggle('bx-x')
 }
 
@@ -29,56 +27,90 @@ splide.mount();
 
 //GALLERY//
 let galleryImages = document.querySelectorAll(".gallery-image");
-let getLatestOpenedImg;
+// let getLatestOpenedImg;
 let windowwidth = window.innerWidth;
+let newImg;
 
 if(galleryImages){
   galleryImages.forEach(function(image, index){
     image.onclick = function() {
      let getFullImgUrl = image.src;
 
-     getLatestOpenedImg = index + 1;
+    //  getLatestOpenedImg = index + 1;
 
      let container = document.body;
      let newFullImgUrlWindow = document.createElement("div");
     container.appendChild(newFullImgUrlWindow);
-    console.log("here")
     newFullImgUrlWindow.setAttribute("class","img-window");
     newFullImgUrlWindow.setAttribute("onclick","closeImg()");
     
-    let newImg = document.createElement("img");
+    newImg = document.createElement("img");
     newFullImgUrlWindow.appendChild(newImg);
     newImg.setAttribute("src", getFullImgUrl);
 
 
-    new.Img.onload = function (){
+    newImg.onload = function (){
+      console.log("Image loading")
       let imgWidth = this.width;
-      let calcImgToEdge =((windowwiidth - imgWidth) / 2) - 80;
+      // let calcImgToEdge =((windowwiidth - imgWidth) / 2) - 80;
 
 
-      let newNextBtn = document.createElement("a");
-      let btnNextText = document.createTextNode("Next");
-      newNextBtn.appendChild(btnNextText);
-      container.appendChild(newNextBtn);
-      newNextBtn.setAttribute("class", "img-btn-prev");
-      newNextBtn.setAttribute("onclick", "changeImg()");
-      newNextBtn.style.cssFloatText = "";
+      // let newNextBtn = document.createElement("a");
+      // let btnNextText = document.createTextNode("Next");
+      // newNextBtn.appendChild(btnNextText);
+      // container.appendChild(newNextBtn);
+      // newNextBtn.setAttribute("class", "img-btn-prev");
+      // newNextBtn.setAttribute("onclick", "changeImg()");
+      // newNextBtn.style.cssFloatText = "Next";
   
-      let newPrevBtn = document.createElement("a");
-      let btnPrevText = document.createTextNode("Prev");
-      newPrevBtn.appendChild(btnPrevText);
-      container.appendChild(newPrevBtn);
-      newPrevBtn.setAttribute("class", "img-btn-prev");
-      newPrevBtn.setAttribute("onclick", "changeImg()");
+      // let newPrevBtn = document.createElement("a");
+      // let btnPrevText = document.createTextNode("Prev");
+      // newPrevBtn.appendChild(btnPrevText);
+      // container.appendChild(newPrevBtn);
+      // newPrevBtn.setAttribute("class", "img-btn-prev");
+      // newPrevBtn.setAttribute("onclick", "changeImg()");
+      // newPrevBtn.style.cssFloatText = "Prev"
   
     }
-    
-
-
     }
   });
 }
 
 function closeImg() {
   document.querySelector(".img-window").remove();
+}
+
+//listen to the arrows
+
+document.addEventListener('keydown', (event) => {
+  const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+  if (key === "ArrowRight") {
+    switchPic("right")
+  } else if (key === "ArrowLeft") {
+    switchPic("left")
+  }
+});
+
+// function to switch picture
+
+function switchPic(direction) {
+  const imagesArray = Array.from(galleryImages)
+ console.log(imagesArray)
+  const currentImageIndex = imagesArray.findIndex((image) => {
+    return image.src === newImg.src
+  })
+
+  if (direction === "right") {
+    if (imagesArray.length === currentImageIndex + 1) {
+      newImg.src = imagesArray[0]
+    } else {
+      newImg.src = imagesArray[currentImageIndex + 1].src
+    }
+  } else if (direction === "left") {
+    if (currentImageIndex === 0) {
+      newImg.src = imagesArray[imagesArray.length - 1]
+    } else {
+      newImg.src = imagesArray[currentImageIndex - 1].src
+    }
+  }
 }
