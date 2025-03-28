2ca6caf98f46968fcc40e8e4246621b54bd5968c
Maria Candelaria Fernandez <candelariafeern@gmail.com>
2025-03-28 12:50:33 -0300

    Responsive Tablez Size

--- javascript.js
+++ javascript.js
@@ -1,29 +1,28 @@
-let menuCloseButtton = document.querySelector('#menu-close-button');
-let menulist = document.querySelector('#menulist');
-let menuOpenButton = document.querySelector ('#menu-open-button')
+let menuCloseButtton = document.querySelector("#menu-close-button");
+let menulist = document.querySelector("#menulist");
+let menuOpenButton = document.querySelector("#menu-open-button");
 
 menuCloseButtton.onclick = () => {
-    menulist.style.setProperty("display", "none")
-    // neceitamos el menu
-    // tenemos que ponerle display none al menu
-    // menu.classList.toggle('bx-x')
-}
+  menulist.style.setProperty("display", "none");
+  // neceitamos el menu
+  // tenemos que ponerle display none al menu
+  // menu.classList.toggle('bx-x')
+};
 
-menuOpenButton.onclick = ()=>{
- menulist.style.setProperty("display", "block")
-}
+menuOpenButton.onclick = () => {
+  menulist.style.setProperty("display", "block");
+};
 
-var splide = new Splide( '.splide', {
-  type    : 'loop',
-   perPage : 3,
-   focus: 'center',
-   autoWidth: true,
-   autoplay: true,
-   height: "500px"
- } );
-  
+var splide = new Splide(".splide", {
+  type: "loop",
+  perPage: 3,
+  focus: "center",
+  autoWidth: true,
+  autoplay: true,
+  height: "500px",
+});
+
 splide.mount();
-
 
 //GALLERY//
 let galleryImages = document.querySelectorAll(".gallery-image");
@@ -31,48 +30,45 @@ let galleryImages = document.querySelectorAll(".gallery-image");
 let windowwidth = window.innerWidth;
 let newImg;
 
-if(galleryImages){
-  galleryImages.forEach(function(image, index){
-    image.onclick = function() {
-     let getFullImgUrl = image.src;
+if (galleryImages) {
+  galleryImages.forEach(function (image, index) {
+    image.onclick = function () {
+      let getFullImgUrl = image.src;
 
-    //  getLatestOpenedImg = index + 1;
+      //  getLatestOpenedImg = index + 1;
 
-     let container = document.body;
-     let newFullImgUrlWindow = document.createElement("div");
-    container.appendChild(newFullImgUrlWindow);
-    newFullImgUrlWindow.setAttribute("class","img-window");
-    newFullImgUrlWindow.setAttribute("onclick","closeImg()");
-    
-    newImg = document.createElement("img");
-    newFullImgUrlWindow.appendChild(newImg);
-    newImg.setAttribute("src", getFullImgUrl);
+      let container = document.body;
+      let newFullImgUrlWindow = document.createElement("div");
+      container.appendChild(newFullImgUrlWindow);
+      newFullImgUrlWindow.setAttribute("class", "img-window");
+      newFullImgUrlWindow.setAttribute("onclick", "closeImg()");
 
+      newImg = document.createElement("img");
+      newFullImgUrlWindow.appendChild(newImg);
+      newImg.setAttribute("src", getFullImgUrl);
 
-    newImg.onload = function (){
-      console.log("Image loading")
-      let imgWidth = this.width;
-      // let calcImgToEdge =((windowwiidth - imgWidth) / 2) - 80;
+      newImg.onload = function () {
+        console.log("Image loading");
+        let imgWidth = this.width;
+        // let calcImgToEdge =((windowwiidth - imgWidth) / 2) - 80;
 
+        // let newNextBtn = document.createElement("a");
+        // let btnNextText = document.createTextNode("Next");
+        // newNextBtn.appendChild(btnNextText);
+        // container.appendChild(newNextBtn);
+        // newNextBtn.setAttribute("class", "img-btn-prev");
+        // newNextBtn.setAttribute("onclick", "changeImg()");
+        // newNextBtn.style.cssFloatText = "Next";
 
-      // let newNextBtn = document.createElement("a");
-      // let btnNextText = document.createTextNode("Next");
-      // newNextBtn.appendChild(btnNextText);
-      // container.appendChild(newNextBtn);
-      // newNextBtn.setAttribute("class", "img-btn-prev");
-      // newNextBtn.setAttribute("onclick", "changeImg()");
-      // newNextBtn.style.cssFloatText = "Next";
-  
-      // let newPrevBtn = document.createElement("a");
-      // let btnPrevText = document.createTextNode("Prev");
-      // newPrevBtn.appendChild(btnPrevText);
-      // container.appendChild(newPrevBtn);
-      // newPrevBtn.setAttribute("class", "img-btn-prev");
-      // newPrevBtn.setAttribute("onclick", "changeImg()");
-      // newPrevBtn.style.cssFloatText = "Prev"
-  
-    }
-    }
+        // let newPrevBtn = document.createElement("a");
+        // let btnPrevText = document.createTextNode("Prev");
+        // newPrevBtn.appendChild(btnPrevText);
+        // container.appendChild(newPrevBtn);
+        // newPrevBtn.setAttribute("class", "img-btn-prev");
+        // newPrevBtn.setAttribute("onclick", "changeImg()");
+        // newPrevBtn.style.cssFloatText = "Prev"
+      };
+    };
   });
 }
 
@@ -82,35 +78,45 @@ function closeImg() {
 
 //listen to the arrows
 
-document.addEventListener('keydown', (event) => {
+document.addEventListener("keydown", (event) => {
   const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
   if (key === "ArrowRight") {
-    switchPic("right")
+    switchPic("right");
   } else if (key === "ArrowLeft") {
-    switchPic("left")
+    switchPic("left");
   }
 });
 
 // function to switch picture
 
 function switchPic(direction) {
-  const imagesArray = Array.from(galleryImages)
- console.log(imagesArray)
+  const imagesArray = Array.from(galleryImages);
+  console.log(imagesArray);
   const currentImageIndex = imagesArray.findIndex((image) => {
-    return image.src === newImg.src
-  })
+    return image.src === newImg.src;
+  });
 
   if (direction === "right") {
     if (imagesArray.length === currentImageIndex + 1) {
-      newImg.src = imagesArray[0]
+      newImg.src = imagesArray[0];
     } else {
-      newImg.src = imagesArray[currentImageIndex + 1].src
+      newImg.src = imagesArray[currentImageIndex + 1].src;
     }
   } else if (direction === "left") {
     if (currentImageIndex === 0) {
-      newImg.src = imagesArray[imagesArray.length - 1]
+      newImg.src = imagesArray[imagesArray.length - 1];
     } else {
-      newImg.src = imagesArray[currentImageIndex - 1].src
+      newImg.src = imagesArray[currentImageIndex - 1].src;
     }
   }
 }
+//const getTitle = async () => {
+//const response = fetch("http://localhost:1337/api/about");
+//const titleData = await response.json();
+//const title = titleData.data.title;
+//console.log("title", title);
+//const titleElement = document.querySelector("#titleElement");
+//titleElement.innerHTML = title;
+// };
+
+// getTitle()
